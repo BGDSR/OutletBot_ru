b1f45c1b308f56c09f2e6af4aa323aeee50268a3
BGDSR <66807066+BGDSR@users.noreply.github.com>
2025-04-18 03:00:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+# config.py
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    raise ValueError("❌ Переменная окружения TOKEN не найдена!")
